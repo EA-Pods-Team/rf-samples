ef97d26f0f901dadbb26f8ec1905ba1c8c8c4950
Christopher Kodama <chkodama@Christophers-MacBook-Air.local>
2019-12-13 00:57:44 -0600

    added lambda function implementation, havent tested yet

--- test.py
+++ test.py
@@ -14,11 +14,15 @@ print(f'Step: {step}')
 #this janky step gets rid of some rounding issues I have
 t = t - DT/100
 
-
+# variable space: PRIs, amplitudes, phase
 pri = 100e-9
 f = 1.0 / pri
+
+waveFcn = lambda A, t, f, phi: A * signal.square(2 * np.pi * f * t - phi)
+
 print(f'Frequency: {f}')
-wave = signal.square(2*np.pi * f * t)
+
+wave = waveFcn(1, t, f, 0)
 
 plt.plot(t,wave)
 plt.show()
